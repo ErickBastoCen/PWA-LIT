c4c9a53d1de6f57ab3bce6696cb1da585c6196ea
ErickBastoCen <erick.19070005@itsmotul.edu.mx>
2023-02-09 23:58:05 -0600

    todo-list-Paso 8-Final

--- Intro to Lit/todo-list.ts
+++ Intro to Lit/todo-list.ts
@@ -17,32 +17,40 @@ export class ToDoList extends LitElement {
 
   @state()
   private _listItems = [
-    { text: 'Make to-do list', completed: true },
+  { text: 'Make to-do list', completed: true },
     { text: 'Complete Lit tutorial', completed: false }
   ];
   @property()
   hideCompleted = false;
 
   render() {
-    // TODO: Replace items definition.
-    const items = this._listItems;
+    const items = this.hideCompleted
+      ? this._listItems.filter((item) => !item.completed)
+      : this._listItems;
     const todos = html`
       <ul>
         ${items.map((item) =>
-          html`
-            <li
-                class=${item.completed ? 'completed' : ''}
-                @click=${() => this.toggleCompleted(item)}>
-              ${item.text}
-            </li>`
+            html`
+              <li
+                  class=${item.completed ? 'completed' : ''}
+                  @click=${() => this.toggleCompleted(item)}>
+                ${item.text}
+              </li>`
         )}
       </ul>
     `;
-    // TODO: Define partial templates.
+    const caughtUpMessage = html`
+      <p>
+      You're all caught up!
+      </p>
+    `;
+    const todosOrMessage = items.length > 0
+      ? todos
+      : caughtUpMessage;
+
     return html`
       <h2>To Do</h2>
-      <!-- TODO: Update expression. -->
-      ${todos}
+      ${todosOrMessage}
       <input id="newitem" aria-label="New item">
       <button @click=${this.addToDo}>Add</button>
       <br>
@@ -52,8 +60,6 @@ export class ToDoList extends LitElement {
           ?checked=${this.hideCompleted}>
         Hide completed
       </label>
-
-
     `;
   }
 
